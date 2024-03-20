dd86507f8ee321420003b16daf1ccd7b16335d25
mukesh <sutharmukesh910@gmail.com>
2024-03-20 18:16:09 +0530

    new commit

---
+++ 14node_REST_APIs_Practice_Application/backend/app.js
@@ -0,0 +1,70 @@
+const path = require("path");
+
+const express = require("express");
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+const multer = require("multer");
+
+const feerRoutes = require("./routes/feed");
+
+const app = express();
+
+const fileStorage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "image/jpg" ||
+    file.mimetype === "image/jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
+// app.use(bodyParser.urlencoded())  // x-mm-form-urlencoded <form>
+app.use(bodyParser.json()); // application/json
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, PATCH, DELETE"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authentication");
+  next();
+});
+
+app.use("/feed", feerRoutes);
+
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
+
+mongoose
+  .connect(
+    "mongodb+srv://nova:nova000@cluster0.uqrrthi.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then((Result) => {
+    console.log("Database connected successfully.");
+    app.listen(8080);
+  })
+  .catch((err) => {
+    console.log("Database connection failed.");
+  });
